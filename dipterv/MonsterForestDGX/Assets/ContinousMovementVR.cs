9b23df3acecaa76ae008a8ecc561e38abe899d93
Dorogi-Kovács Gábor <dorogi.x@gmail.com>
2020-08-29 13:35:55 +0200

    Update Continous Movement

--- dipterv/MonsterForestDGX/Assets/ContinousMovementVR.cs
+++ dipterv/MonsterForestDGX/Assets/ContinousMovementVR.cs
@@ -6,41 +6,69 @@ public class ContinousMovementVR : MonoBehaviour
 {
     public Player player;
     public XRNode input;
-    private Vector2 inputDirection;
+    private Vector2 inputAxis;
     public Rigidbody rigid;
+    public CharacterController character;
+
     public float speed = 3;
+    public float gravity = -9.81f;
+    private float fallingSpeed = 0f;
 
     public GameObject body;
-    public XRRig xrRig;
-
-    public BoxCollider playerCollider;
+    public XRRig rig;
 
     private readonly float additionalHeight = 0.01f;
+
+    public LayerMask groundLayer;
 
     void Update()
     {
         if (player.CanMove())
         {
             InputDevice device = InputDevices.GetDeviceAtXRNode(input);
-            device.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputDirection);
+            device.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxis);
         }
-    }
-
-    private void ColliderFollowHeadset()
-    {
-        playerCollider.size = new Vector3(playerCollider.size.x, xrRig.cameraInRigSpaceHeight, playerCollider.size.z);
-        Vector3 center = transform.InverseTransformPoint(xrRig.cameraGameObject.transform.position);
-        playerCollider.center = new Vector3(center.x, playerCollider.size.y / 2 + additionalHeight, center.z);
     }
 
     private void FixedUpdate()
     {
-        ColliderFollowHeadset();
+        CapsuleFollowHeadset();
 
-        Quaternion rotation = Quaternion.Euler(0, xrRig.cameraGameObject.transform.eulerAngles.y, 0);
-        Vector3 position = transform.position + (rotation * new Vector3(inputDirection.x, 0, inputDirection.y) * Time.fixedDeltaTime * speed);
-        rigid.MovePosition(position);
+        Quaternion headYaw = Quaternion.Euler(0, rig.cameraGameObject.transform.eulerAngles.y, 0);
+        Vector3 direction = headYaw * new Vector3(inputAxis.x, 0, inputAxis.y);
 
-        inputDirection = Vector2.zero;
+        character.Move(direction * Time.fixedDeltaTime * speed);
+
+        Gravity();
+    }
+
+    private void Gravity()
+    {
+        if (CheckIfGrounded())
+        {
+            fallingSpeed = 0;
+        }
+        else
+        {
+            fallingSpeed += gravity * Time.fixedDeltaTime;
+        }
+
+        character.Move(Vector3.up * fallingSpeed * Time.fixedDeltaTime);
+    }
+
+    private bool CheckIfGrounded()
+    {
+        Vector3 rayStart = transform.TransformPoint(character.center);
+        float rayLength = character.center.y + 0.01f;
+        bool hasHit = Physics.SphereCast(rayStart, character.radius, Vector3.down, out RaycastHit hitInfo, rayLength, groundLayer);
+
+        return hasHit;
+    }
+
+    private void CapsuleFollowHeadset()
+    {
+        character.height = rig.cameraInRigSpaceHeight + additionalHeight;
+        Vector3 capsuleCenter = transform.InverseTransformPoint(rig.cameraGameObject.transform.position);
+        character.center = new Vector3(capsuleCenter.x, character.height / 2 + character.skinWidth, capsuleCenter.z);
     }
 }
